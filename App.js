e7c7260d169e7ea0f70256efda364f151129e1a9
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-07-08 19:38:56 +0530

    introduction to jsx

--- App.js
+++ App.js
@@ -2,41 +2,51 @@ import React from "react";
 import  ReactDOM  from "react-dom/client";
  
 
-const heading = React.createElement(
-    "div",
-    {
-        id:"container",
-    },
-    [
-        React.createElement(
-        "h1",
-        {
-            id: 'title',
-            key:"h1"
-        }
-        ,
-        "Namaste React"
-    ), 
-    React.createElement(
-        "h2",
-        {
-            id: 'title',
-            key:"h2"
-        }
-        ,
-        "Namaste React 2"
-    ),
-    React.createElement(
-        "h3",
-        {
-            id: 'title',
-            key:"h3"
-        }
-        ,
-        "Namaste React 3"
-    )
-    ]
-);
+// const heading = React.createElement(
+//     "div",
+//     {
+//         id:"container",
+//     },
+//     [
+//         React.createElement(
+//         "h1",
+//         {
+//             id: 'title',
+//             key:"h1"
+//         }
+//         ,
+//         "Namaste React"
+//     ), 
+//     React.createElement(
+//         "h2",
+//         {
+//             id: 'title',
+//             key:"h2"
+//         }
+//         ,
+//         "Namaste React 2"
+//     ),
+//     React.createElement(
+//         "h3",
+//         {
+//             id: 'title',
+//             key:"h3"
+//         }
+//         ,
+//         "Namaste React 3"
+//     )
+//     ]
+// );
+
+
+
+// same code using JSX
+
+const heading =(
+    <h1 id='title' key='h1'>
+        Namaste React
+    </h1>
+)
 
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
