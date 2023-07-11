a508bd564c2514d552ecf4aa1cbbcc8c48a8ecf7
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-07-11 11:43:32 +0530

    made new file for Title and added it to the component folder and added the app.js file to the src folder creating the react file struvture

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Title from "./component/Title";
 
-const Title = () => (
-  <a href="/">
-    <img
-      className="Logo"
-      alt="Food_Villa_Logo"
-      src="https://scontent.fbbi6-1.fna.fbcdn.net/v/t39.30808-6/300568234_1119074258685043_4997319920694524626_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=eeHdQrIHvrEAX_K6ih4&_nc_ht=scontent.fbbi6-1.fna&oh=00_AfBORaTnIBDdJuIMLje038Hq8xBqMLxNmJSf9GfpFyur9Q&oe=64B1FDDD"
-    />
-  </a>
-);
-
-//Components
-//-->Functional component
-//-->class Based component
-
-//functional component
+//Header
 const HeaderComponent = () => (
   <div className="header">
     <Title />
@@ -31,10 +18,6 @@ const HeaderComponent = () => (
 );
 
 //Body
-
-//CONFIG DRIVEN UI
-
-//ABOVE IS THE STRUCTURE OF CONFIG DRIVEN UI
 
 const RestaurantList = [
   {
