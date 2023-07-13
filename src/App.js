fc8a71f36966105ec53619886b77dd811a586ee4
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-07-13 10:56:21 +0530

    added the about page and Introduction to the router added the router to the about page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import ReactDOM from "react-dom/client";
 import Header from "./component/Header.js";
 import Body from "./component/Body.js";
 import Footer from "./component/Footer.js";
-
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import About from "./component/About.js";
 //Body
 
 const AppLayout = () => {
@@ -17,7 +18,18 @@ const AppLayout = () => {
   );
 };
 
+const approuter=createBrowserRouter([
+  {
+    path:"/",
+    element:<AppLayout/>
+  },
+  {
+    path:'/about',
+    element:<About/>
+  }
+])
+
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={approuter}/>);
