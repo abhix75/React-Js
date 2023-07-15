85c30fcf765f51f49e5a5673a63109a67a98d34f
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-07-15 11:13:59 +0530

    Adding States Variable in Functional Component

--- dist/index.7271efb6.js
+++ dist/index.7271efb6.js
@@ -35501,14 +35501,18 @@ try {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 var _jsxDevRuntime = require("react/jsx-dev-runtime");
+var _react = require("react");
+var _s = $RefreshSig$();
 const Profile = (props)=>{
+    _s();
+    const [count] = (0, _react.useState)(0);
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
         children: [
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h1", {
                 children: "Profile From Functional Component"
             }, void 0, false, {
                 fileName: "src/component/Profile.js",
-                lineNumber: 3,
+                lineNumber: 5,
                 columnNumber: 7
             }, undefined),
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h3", {
@@ -35518,16 +35522,27 @@ const Profile = (props)=>{
                 ]
             }, void 0, true, {
                 fileName: "src/component/Profile.js",
-                lineNumber: 4,
+                lineNumber: 6,
+                columnNumber: 7
+            }, undefined),
+            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h3", {
+                children: [
+                    "Count:",
+                    count
+                ]
+            }, void 0, true, {
+                fileName: "src/component/Profile.js",
+                lineNumber: 7,
                 columnNumber: 7
             }, undefined)
         ]
     }, void 0, true, {
         fileName: "src/component/Profile.js",
-        lineNumber: 2,
+        lineNumber: 4,
         columnNumber: 10
     }, undefined);
 };
+_s(Profile, "Y89bt/pi8lrdHE1hdS9fijgV/R0=");
 _c = Profile;
 exports.default = Profile;
 var _c;
@@ -35538,7 +35553,7 @@ $RefreshReg$(_c, "Profile");
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-dev-runtime":"iTorj","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru"}],"cosKY":[function(require,module,exports) {
+},{"react/jsx-dev-runtime":"iTorj","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru","react":"21dqq"}],"cosKY":[function(require,module,exports) {
 var $parcel$ReactRefreshHelpers$ea6f = require("@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js");
 var prevRefreshReg = window.$RefreshReg$;
 var prevRefreshSig = window.$RefreshSig$;
--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,8 +1,13 @@
+import { useState } from "react";
+
 const Profile = (props) => {
+  const [count] = useState(0);
+
   return (
     <div>
       <h1>Profile From Functional Component</h1>
       <h3>Name:{props.name}</h3>
+      <h3>Count:{count}</h3>
     </div>
   );
 };
